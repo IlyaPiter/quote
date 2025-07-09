3e80f321008b5e3fe27774c6b3e1a7a90ddb7449
IlyaPiter <ilyak@bk.ru>
2025-07-09 18:25:44 +0300

    final comment

--- quote_generator/quote_generator/settings.py
+++ quote_generator/quote_generator/settings.py
@@ -17,7 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -28,10 +27,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-
-INTERNAL_IPS = ['127.0.0.1',]
 
 ROOT_URLCONF = 'quote_generator.urls'
 
--- quote_generator/quote_generator/urls.py
+++ quote_generator/quote_generator/urls.py
@@ -1,12 +1,6 @@
-from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('', include('page.urls')),
     path('admin/', admin.site.urls),
-]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
